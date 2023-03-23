1a080870a6cb5e6a3e55c5e12339198f4479307d
lonileere <32738539+lonileere@users.noreply.github.com>
2023-03-23 20:48:34 +0000

    ✨ link to first project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,16 @@ export default function Projects() {
   return (
     <ul className="text-xl">
       <li className="flex flex-col">
-        <span>
-          Example Project <BsArrowUpRight className="inline-block ml-1" />{" "}
-        </span>
-        <span className="text-gray-400 font-light">
-          Show Repo <BsArrowUpRight className="inline-block ml-1" />
-        </span>
+        <Link to="/weatherapp">
+          <span>
+            Weather App <BsArrowUpRight className="inline-block ml-1" />{" "}
+          </span>
+        </Link>
+        <Link to="https://github.com/lonileere/weatherapp">
+          <span className="text-gray-400 font-light">
+            Show Repo <BsArrowUpRight className="inline-block ml-1" />
+          </span>
+        </Link>
       </li>
       <li className="flex flex-col">
         <span>
